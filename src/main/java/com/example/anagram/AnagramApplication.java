4c88226f633597fb1aa5417b211f781a486640a8
GhostMadness <ghostmadness55@gmail.com>
2023-05-22 20:29:20 +0300

    добавил приветствие и запрос строки в классе AnagramApplication, в классе AnagramCreater создал метод validate, которые проверяет строку на пустоту и пробелы, и немного поменял оформление.

--- src/main/java/com/example/anagram/AnagramApplication.java
+++ src/main/java/com/example/anagram/AnagramApplication.java
@@ -6,6 +6,8 @@ public class AnagramApplication {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         AnagramCreater anagram = new AnagramCreater();
+        System.out.println("Вас приветсвует программа анаграмм.");
+        System.out.println("Введите строку:");
         System.out.println(anagram.createAnagram(scanner.nextLine()));
     }
 }
--- src/main/java/com/example/anagram/AnagramCreater.java
+++ src/main/java/com/example/anagram/AnagramCreater.java
@@ -2,20 +2,21 @@ package src.main.java.com.example.anagram;
 
 public class AnagramCreater {
     public String createAnagram(String anagram){
+        validate(anagram);
         StringBuilder result = new StringBuilder();
-        String[] anagrams = anagram.split(" ");
+        String[] anagrams = anagram.split("\s");
         for (String line : anagrams) {
-            if (!(result.isEmpty())){
+            if (!(result.isEmpty()) && result.charAt(result.length() - 1) != ' '){
                 result.append(" ");
             }
-            StringBuilder beta = new StringBuilder(getString(line));
-            beta.reverse();
-            for (int i = 0, j = 0; i < line.length(); i++){
-                if ((int) 'A' <= line.charAt(i) && line.charAt(i) <= (int) 'z') {
-                    result.append(beta.charAt(j));
-                    j++;
+            StringBuilder reverseLine = new StringBuilder(getString(line));
+            reverseLine.reverse();
+            for (int index = 0, index2 = 0; index < line.length(); index++){
+                if (Character.isLetter(line.charAt(index))) {
+                    result.append(reverseLine.charAt(index2));
+                    index2++;
                 } else{
-                    result.append(line.charAt(i));
+                    result.append(line.charAt(index));
                 }
             }
         }
@@ -24,11 +25,19 @@ public class AnagramCreater {
 
     private String getString(String line){
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < line.length(); i++){
-            if ((int) 'A' <= line.charAt(i) && line.charAt(i) <= (int) 'z'){
-                result.append(line.charAt(i));
+        for (int index = 0; index < line.length(); index++){
+            if (Character.isLetter(line.charAt(index))){
+                result.append(line.charAt(index));
             }
         }
         return result.toString();
+    }
+
+    private void validate(String string) throws RuntimeException{
+        if (string.isEmpty()){
+            throw new RuntimeException("передана пустая строка");
+        } else if (string.trim().length() == 0) {
+            throw new RuntimeException("строка состоит из пробелов");
+        }
     }
 }
